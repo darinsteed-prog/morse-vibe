fc2fa9090d433592424b624f19c7983990016ef8
darin <darin.steed@gmail.com>
2026-07-05 17:57:36 +0100

    Remove patch scripts

--- fix-version4.cjs
+++
@@ -1,7 +0,0 @@
-﻿const fs = require("fs");
-const file = "android/app/build.gradle";
-let src = fs.readFileSync(file, "utf8");
-src = src.replace("versionCode 4", "versionCode 5");
-src = src.replace('versionName "1.3"', 'versionName "1.0"');
-fs.writeFileSync(file, src, "utf8");
-console.log("✔ Version 5 (1.0)");
