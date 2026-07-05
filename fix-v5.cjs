e868b25c9d510bfcae1ba46488e419fef42c428e
darin <darin.steed@gmail.com>
2026-07-05 17:56:34 +0100

    Fix Pro paywall - gate Air and Remote tabs properly

---
+++ fix-v5.cjs
@@ -0,0 +1,7 @@
+﻿const fs = require("fs");
+const file = "android/app/build.gradle";
+let src = fs.readFileSync(file, "utf8");
+src = src.replace("versionCode 5", "versionCode 6");
+src = src.replace('versionName "1.0"', 'versionName "1.1"');
+fs.writeFileSync(file, src, "utf8");
+console.log("✔ Version 6 (1.1)");
