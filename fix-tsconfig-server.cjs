5a4ecf9694d202e14e71204db43bfd1785e3674f
darin <darin.steed@gmail.com>
2026-08-26 18:44:10 +0100

    Fix server TypeScript config - disable strict mode

---
+++ fix-tsconfig-server.cjs
@@ -0,0 +1,13 @@
+﻿const fs = require("fs");
+const file = "tsconfig.server.json";
+let src = fs.readFileSync(file, "utf8");
+
+src = src.replace(
+  `"allowImportingTsExtensions": false`,
+  `"allowImportingTsExtensions": false,
+    "strict": false,
+    "noImplicitAny": false`
+);
+
+fs.writeFileSync(file, src, "utf8");
+console.log("✔ Fixed tsconfig.server.json");
